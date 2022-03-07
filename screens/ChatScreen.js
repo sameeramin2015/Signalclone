a5953fec88279fac0e72b1cacb3cfd40bdabf0bc
sameeramin2015 <sameeramin2015@gmail.com>
2022-03-07 14:06:41 +0100

    update

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { Avatar } from "react-native-elements";
 
 const ChatScreen = ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: "Chat",
+      headerBackTitleVisible: false,
+      headerTitleAlign: "left",
+      headerTitle: () => (
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <Avatar
+            rounded
+            source={{
+              uri: "https://connectingcouples.us/wp-content/uploads/2019/07/avatar-placeholder.png",
+            }}
+          />
+          <Text>{route.params.chatName}</Text>
+        </View>
+      ),
+    });
+  }, [navigation]);
   return (
     <View>
       <Text>{route.params.chatName}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ChatScreen
+export default ChatScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
